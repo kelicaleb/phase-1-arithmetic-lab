76d4b6ee8f0fced67b6d08d2c7a2bde009dfffc4
kelicaleb <kelicaleb7@gmail.com>
2022-08-22 01:54:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+console.log(multiply);
+var random = Math.round(Math.random+1);
+const num3 = 5;
+const num4 = 24;
+var mod = 24 % 5;
+console.log(mod);
+const num = (1, 2, 3, 5 , 14, 20);
+const max = Math.max(num);
+console.log(max);
 
-// Write your code here
